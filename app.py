947b91216383fb981333a2a4a0bd3ee34185ccee
Alex Taber <alexftaber@gmail.com>
2018-09-04 21:36:20 -0400

    Various Improvements
    Convert del tags to ~~
    Use usernames rather than nicknames on the matrix side
    Attachments use the same prefix as normal messages

--- app.py
+++ app.py
@@ -29,12 +29,17 @@ matrix_room = matrix_client.join_room(matrix_room_id)
 
 matrix_file_types = ('m.file', 'm.image', 'm.video', 'm.audio')
 
+def prepare_matrix_client(message):
+	attachments = "\n".join([x.url for x in message.attachments])
+	content = message.clean_content + ("\n" + attachments if attachments != "" else "")
+	return content
+
 @discord_client.event
 async def on_message(message):
 	if message.author.discriminator == "0000" or message.channel.id != discord_channel: return
-	username = message.author.display_name[:1] + "\u200B" + message.author.display_name[1:]
-	attachments = "\n".join([x.url for x in message.attachments])
-	matrix_room.send_text("<{}> {}".format(username, message.clean_content + ("\n" + attachments if attachments != "" else "")))
+	username = message.author.name[:1] + "\u200B" + message.author.name[1:] + "#" + message.author.discriminator
+	content = prepare_matrix_client(message)
+	matrix_room.send_text("<{}> {}".format(username, content))
 
 def send_webhook(username, avatar_url, content):
 	data = {'username': username, 'content': content}
@@ -45,6 +50,7 @@ def send_webhook(username, avatar_url, content):
 def prepare_discord_content(content):
 	content = content.replace("@everyone", "@\u200Beveryone")
 	content = content.replace("@here", "@\u200Bhere")
+	content = re.sub("</?del>", "~~", content)
 	mentions = re.findall("(^|\s)(@(\w*))", content)
 	guild = discord_client.get_channel(discord_channel).guild
 	for mention in mentions:
@@ -62,7 +68,7 @@ def on_matrix_message(room, event):
 			content = prepare_discord_content(event['content']['body'])
 			send_webhook(username, avatar, content)
 		if event['content']['msgtype'] in matrix_file_types:
-			username = "[Matrix] {}".format(user.get_display_name())
+			username = "{}{}".format(discord_prefix, user.get_display_name())
 			avatar = user.get_avatar_url()
 			content = matrix_homeserver + "/_matrix/media/v1/download/" + event['content']['url'][6:]
 			send_webhook(username, avatar, content)
