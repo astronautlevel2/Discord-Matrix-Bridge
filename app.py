7d80aa5d8d438ed470f3eb68969aefa27a47a499
Alex Taber <alexftaber@gmail.com>
2018-09-06 12:04:08 -0400

    Delete previous message when edited, carry deletes from discord

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from matrix_client.client import MatrixClient
 import requests
 import json
 import discord
+from discord import Webhook, RequestsWebhookAdapter
 import re
 from config import *
 
@@ -29,6 +30,8 @@ matrix_room = matrix_client.join_room(matrix_room_id)
 
 matrix_file_types = ('m.file', 'm.image', 'm.video', 'm.audio')
 
+message_id_cache = {}
+
 def prepare_matrix_content(message):
 	attachments = "\n".join([x.url for x in message.attachments])
 	content = message.clean_content + ("\n" + attachments if attachments != "" else "")
@@ -36,34 +39,41 @@ def prepare_matrix_content(message):
 
 guild = None
 emojis = {}
+webhook = None
 
 @discord_client.event
 async def on_ready():
 	global guild
 	global emojis
+	global webhook
 	guild = discord_client.get_channel(discord_channel).guild
 	emojis = {":{}:".format(emoji.name): "<:{}:{}>".format(emoji.name, emoji.id) for emoji in guild.emojis}
+	webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter())
 
 @discord_client.event
 async def on_message(message):
 	if message.author.discriminator == "0000" or message.channel.id != discord_channel: return
 	username = message.author.name[:1] + "\u200B" + message.author.name[1:] + "#" + message.author.discriminator
 	content = prepare_matrix_content(message)
-	matrix_room.send_text("<{}> {}".format(username, content))
+	matrix_message_id = matrix_room.send_text("<{}> {}".format(username, content))['event_id']
+	message_id_cache[message.id] = matrix_message_id
 
 @discord_client.event
 async def on_message_edit(before, after):
-	if after.channel.id != discord_channel: return
+	if after.author.discriminator == "0000" or after.channel.id != discord_channel: return
+	if after.content == before.content: return
 	after.attachments = []
 	username = after.author.name[:1] + "\u200B" + after.author.name[1:] + "#" + after.author.discriminator
 	content = prepare_matrix_content(after)
-	matrix_room.send_text("<{}> {} (edited)".format(username, content))
+	matrix_room.redact_message(message_id_cache[before.id], reason="Message Edited")
+	message_id_cache[after.id] = matrix_room.send_text("<{}> {} (edited)".format(username, content))['event_id']
+
+@discord_client.event
+async def on_raw_message_delete(paylod):
+	matrix_room.redact_message(message_id_cache[paylod.message_id], reason="Message Deleted")
 
 def send_webhook(username, avatar_url, content):
-	data = {'username': username, 'content': content}
-	if avatar_url: data['avatar_url'] = avatar_url
-	headers = {'Content-type': 'application/json'}
-	r = requests.post(webhook_url, data = json.dumps(data), headers=headers)
+	webhook.send(content=content, username=username, avatar_url=avatar_url)
 
 def prepare_discord_content(content):
 	content = content.replace("@everyone", "@\u200Beveryone")
@@ -86,12 +96,12 @@ def on_matrix_message(room, event):
 			username = "{}{}".format(discord_prefix, user.get_display_name())
 			avatar = user.get_avatar_url()
 			content = prepare_discord_content(event['content']['body'])
-			send_webhook(username, avatar, content)
+			message_id_cache[event['event_id']] = send_webhook(username, avatar, content)
 		if event['content']['msgtype'] in matrix_file_types:
 			username = "{}{}".format(discord_prefix, user.get_display_name())
 			avatar = user.get_avatar_url()
 			content = matrix_homeserver + "/_matrix/media/v1/download/" + event['content']['url'][6:]
-			send_webhook(username, avatar, content)
+			message_id_cache[event['event_id']] = send_webhook(username, avatar, content)
 
 matrix_room.add_listener(on_matrix_message)
 matrix_client.start_listener_thread()
